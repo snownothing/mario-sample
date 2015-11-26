ab760e4e2ad1abdd261da30f8a722abbf52d3a87
biezhi <biezhi.me@gmail.com>
2015-11-26 17:36:39 +0800

    add CRUD

--- src/main/java/com/mario/demo/App.java
+++ src/main/java/com/mario/demo/App.java
@@ -15,7 +15,21 @@ public class App implements Bootstrap {
 		mario.addRoute("/", "index", index);
 		mario.addRoute("/hello", "hello", index);
 		mario.addRoute("/html", "html", index);
+		
 		mario.addRoute("/users", "users", userController);
+		mario.addRoute("/user/add", "show_add", userController);
+		mario.addRoute("/user/save", "save", userController);
+		mario.addRoute("/user/edit", "edit", userController);
+		mario.addRoute("/user/update", "update", userController);
+		mario.addRoute("/user/del", "delete", userController);
+		
+		try {
+			// 解决tomcat下报错
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		// 配置数据库
 		MarioDb.init("jdbc:mysql://127.0.0.1:3306/mario_sample", "root", "root");
--- src/main/java/com/mario/demo/controller/UserController.java
+++ src/main/java/com/mario/demo/controller/UserController.java
@@ -1,21 +1,136 @@
 package com.mario.demo.controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import com.junicorn.mario.MarioContext;
 import com.junicorn.mario.db.MarioDb;
 import com.junicorn.mario.servlet.wrapper.Request;
 import com.junicorn.mario.servlet.wrapper.Response;
 import com.mario.demo.model.User;
 
+/**
+ * 用户控制器
+ */
 public class UserController {
 	
+	/**
+	 * 用户列表
+	 * @param request
+	 * @param response
+	 */
 	public void users(Request request, Response response){
 		List<User> users = MarioDb.getList("select * from t_user", User.class);
-		if(null != users && users.size() > 0){
-			for(User user : users){
-				System.out.println(user);
-			}
+		request.attr("users", users);
+		response.render("users");
+	}
+	
+	/**
+	 * 添加用户界面
+	 * @param request
+	 * @param response
+	 */
+	public void show_add(Request request, Response response){
+		response.render("user_add");
+	}
+	
+	/**
+	 * 保存方法
+	 * @param request
+	 * @param response
+	 * @throws ParseException
+	 */
+	public void save(Request request, Response response) throws ParseException{
+		String name = request.query("name");
+		Integer age = request.queryAsInt("age");
+		String date = request.query("birthday");
+		
+		if(null == name || null == age || null == date){
+			request.attr("res", "error");
+			response.render("user_add");
+			return;
+		}
+		
+		Date bir = new SimpleDateFormat("yyyy-MM-dd").parse(date);
+		
+		int res = MarioDb.insert("insert into t_user(name, age, birthday)", name, age, bir);
+		if(res > 0){
+			String ctx = MarioContext.me().getContext().getContextPath();
+			String location = ctx + "/users";
+			response.redirect(location.replaceAll("[/]+", "/"));
+		} else {
+			request.attr("res", "error");
+			response.render("user_add");
 		}
 	}
 	
+	/**
+	 * 编辑页面
+	 * @param request
+	 * @param response
+	 */
+	public void edit(Request request, Response response){
+		Integer id = request.queryAsInt("id");
+		if(null != id){
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("id", id);
+			User user = MarioDb.get("select * from t_user where id = :id", User.class, map);
+			request.attr("user", user);
+			response.render("user_edit");
+		}
+	}
+	
+	/**
+	 * 修改信息
+	 * @param request
+	 * @param response
+	 */
+	public void update(Request request, Response response){
+		Integer id = request.queryAsInt("id");
+		String name = request.query("name");
+		Integer age = request.queryAsInt("age");
+		
+		if(null == id || null == name || null == age ){
+			request.attr("res", "error");
+			response.render("user_edit");
+			return;
+		}
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("id", id);
+		map.put("name", name);
+		map.put("age", age);
+		
+		int res = MarioDb.update("update t_user set name = :name, age = :age where id = :id", map);
+		if(res > 0){
+			String ctx = MarioContext.me().getContext().getContextPath();
+			String location = ctx + "/users";
+			response.redirect(location.replaceAll("[/]+", "/"));
+		} else {
+			request.attr("res", "error");
+			response.render("user_edit");
+		}
+	}
+	
+	/**
+	 * 删除
+	 * @param request
+	 * @param response
+	 */
+	public void delete(Request request, Response response){
+		Integer id = request.queryAsInt("id");
+		if(null != id){
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("id", id);
+			MarioDb.update("delete from t_user where id = :id", map);
+		}
+		
+		String ctx = MarioContext.me().getContext().getContextPath();
+		String location = ctx + "/users";
+		response.redirect(location.replaceAll("[/]+", "/"));
+	}
 }
